7a1c1b9f9ef75a4f83c5d196bb942a8c72b8b25a
Istiak Ferdous <30789544+istiak101@users.noreply.github.com>
2020-10-01 15:08:29 -0700

    Update version

--- install.sh
+++ install.sh
@@ -64,7 +64,7 @@ chown root:$giteauser /etc/gitea
 chmod 770 /etc/gitea
 
 # Download & Install Gitea
-wget -O gitea https://dl.gitea.io/gitea/1.12.4/gitea-1.12.4-linux-amd64
+wget -O gitea https://dl.gitea.io/gitea/1.12.5/gitea-1.12.5-linux-amd64
 chmod +x gitea
 cp gitea /usr/local/bin/gitea
 
